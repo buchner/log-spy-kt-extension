db000d21dc0bae66bd27dd81f41c77fbf869b24e
Bjoern Buchner <bjoern.buchner@torommo.net>
2020-02-09 13:40:03 +0100

    Publish snapshots to Maven central

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,10 @@
+
+import org.jetbrains.dokka.gradle.DokkaTask
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 buildscript {
     repositories {
         mavenCentral()
-        maven(url = "https://dl.bintray.com/kotlin/dokka")
     }
 
     dependencies {
@@ -12,16 +13,27 @@ buildscript {
     }
 }
 
+plugins {
+    `java-library`
+    `maven-publish`
+    signing
+}
+
 allprojects {
     group = "net.torommo.logspy"
     version = "0.8.0-SNAPSHOT"
 
     repositories {
+        maven(url = "https://dl.bintray.com/kotlin/dokka")
         mavenCentral()
     }
 }
 
 subprojects {
+    apply(plugin = "java-library")
+    apply(plugin = "maven-publish")
+    apply(plugin = "signing")
+    apply(plugin = "org.jetbrains.dokka")
 
     tasks.withType<KotlinCompile> {
         kotlinOptions.jvmTarget = "1.8"
@@ -29,5 +41,76 @@ subprojects {
 
     tasks.withType<Test> {
         useJUnitPlatform()
+    }
+
+    tasks.withType<GenerateModuleMetadata> {
+        enabled = false
+    }
+
+    val dokka by tasks.getting(DokkaTask::class) {
+        outputFormat = "html"
+        outputDirectory = "$buildDir/javadoc"
+    }
+
+    val dokkaJar by tasks.creating(Jar::class) {
+        group = JavaBasePlugin.DOCUMENTATION_GROUP
+        description = "Assembles Kotlin documentation with Dokka."
+        archiveClassifier.set("javadoc")
+        from(dokka)
+    }
+    artifacts.add("archives", dokkaJar)
+
+    val sourcesJar by tasks.creating(Jar::class) {
+        group = JavaBasePlugin.DOCUMENTATION_GROUP
+        description = "Assembles sources."
+        archiveClassifier.set("sources")
+        from(sourceSets.getByName("main").allSource)
+    }
+    artifacts.add("archives", sourcesJar)
+
+    publishing {
+        publications {
+            create<MavenPublication>("mavenJava") {
+                from(components["java"])
+                artifact(dokkaJar)
+                artifact(sourcesJar)
+                pom {
+                    url.set("https://github.com/buchner/log-spy-kt-extension")
+                    licenses {
+                        license {
+                            name.set("GNU LESSER GENERAL PUBLIC LICENSE, Version 3.0")
+                            url.set("https://www.gnu.org/licenses/lgpl-3.0.en.html")
+                        }
+                    }
+                    developers {
+                        developer {
+                            id.set("buchner")
+                            name.set("Björn Buchner")
+                        }
+                    }
+                    scm {
+                        connection.set("scm:git:https://github.com/buchner/log-spy-kt-extension.git")
+                        url.set("https://github.com/buchner/log-spy-kt-extension")
+                    }
+                }
+            }
+        }
+        repositories {
+            maven {
+                name = "sonatype"
+                url = uri("https://oss.sonatype.org/content/repositories/snapshots")
+                credentials {
+                    username = project.property("mavenUser") as String?
+                    password = project.property("mavenPassword") as String?
+                }
+            }
+        }
+    }
+
+    signing {
+        val signingKey = project.property("signingKey") as String?
+        val signingPassword = project.property("signingPassword") as String?
+        useInMemoryPgpKeys(signingKey, signingPassword)
+        sign(publishing.publications["mavenJava"])
     }
 }
