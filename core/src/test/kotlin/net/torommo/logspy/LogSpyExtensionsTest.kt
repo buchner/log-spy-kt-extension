e91b139c36f919effac09836acee99483fc9f52c
Bjoern Buchner <bjoern.buchner@torommo.net>
2020-07-12 10:23:51 +0200

    Improve test readability
    - Reduced redundancy
    - Use Kotest inspectors

--- core/src/test/kotlin/net/torommo/logspy/LogSpyExtensionsTest.kt
+++ core/src/test/kotlin/net/torommo/logspy/LogSpyExtensionsTest.kt
@@ -1,6 +1,7 @@
 package net.torommo.logspy.kotest
 
 import io.kotest.core.spec.style.FreeSpec
+import io.kotest.inspectors.forAll
 import io.kotest.matchers.Matcher
 import io.kotest.matchers.MatcherResult
 import io.kotest.matchers.collections.containExactly
@@ -16,145 +17,151 @@ internal class LogSpyExtensionsTest : FreeSpec() {
     init {
         "spy by type" - {
             "creates spy for type" - {
-                val provider = FakeSpyProvider()
-                ServiceLoaderWrapper.predefine<SpyProvider>(provider)
+                useFakeSpyProvider { provider ->
+                    val spy = spyForLogger<TestObject> {
+                        provider.addEvent(TestObject::class, event())
+                    }
 
-                val spy = spyForLogger<ObjectA> {
-                    provider.addEvent(ObjectA::class, SpiedEvent("Test", SpiedEvent.Level.DEBUG, null, emptyMap()))
+                    spy.events() should containExactly(event())
                 }
-
-                spy.events() should containExactly(SpiedEvent("Test", SpiedEvent.Level.DEBUG, null, emptyMap()))
             }
 
             "closes underlying spy after block" - {
-                val provider = FakeSpyProvider()
-                ServiceLoaderWrapper.predefine<SpyProvider>(provider)
+                useFakeSpyProvider { provider ->
+                    spyForLogger<TestObject> {
+                    }
 
-                spyForLogger<ObjectA> {
+                    provider.allInstancesFor(TestObject::class).forAll {
+                        it.shouldBeClosed()
+                    }
                 }
-
-                provider.allInstancesFor(ObjectA::class).first() should beClosed()
             }
 
             "takes snapshot of recorded events" - {
-                val provider = FakeSpyProvider()
-                ServiceLoaderWrapper.predefine<SpyProvider>(provider)
+                useFakeSpyProvider { provider ->
+                    val spy = spyForLogger<TestObject> {
+                        provider.addEvent(TestObject::class, event1())
+                    }
+                    provider.addEvent(TestObject::class, event2())
 
-                val spy = spyForLogger<ObjectA> {
-                    provider.addEvent(ObjectA::class, SpiedEvent("Test 1", SpiedEvent.Level.DEBUG, null, emptyMap()))
+                    spy.events() should containExactly(event1())
                 }
-                provider.addEvent(ObjectA::class, SpiedEvent("Test 2", SpiedEvent.Level.DEBUG, null, emptyMap()))
-
-                spy.events() should containExactly(SpiedEvent("Test 1", SpiedEvent.Level.DEBUG, null, emptyMap()))
             }
 
             "creates new spy for each request" - {
-                val provider = FakeSpyProvider()
-                ServiceLoaderWrapper.predefine<SpyProvider>(provider)
+                useFakeSpyProvider { provider ->
+                    spyForLogger<TestObject> {
+                        provider.addEvent(TestObject::class, event1())
+                        val spy = spyForLogger<TestObject> {
+                            provider.addEvent(TestObject::class, event2())
+                        }
 
-                spyForLogger<ObjectA> {
-                    provider.addEvent(ObjectA::class, SpiedEvent("Test 1", SpiedEvent.Level.DEBUG, null, emptyMap()))
-                    val spy = spyForLogger<ObjectA> {
-                        provider.addEvent(ObjectA::class, SpiedEvent("Test 2", SpiedEvent.Level.DEBUG, null, emptyMap()))
+                        spy.events() should containExactly(event2())
                     }
-
-                    spy.events() should containExactly(SpiedEvent("Test 2", SpiedEvent.Level.DEBUG, null, emptyMap()))
                 }
             }
 
             "delayed spying" - {
                 "creates spy for block" - {
-                    val provider = FakeSpyProvider()
-                    ServiceLoaderWrapper.predefine<SpyProvider>(provider)
-                    val spy = spyForLogger<ObjectA>()
-                    provider.addEvent(ObjectA::class, SpiedEvent("Test 1", SpiedEvent.Level.DEBUG, null, emptyMap()))
+                    useFakeSpyProvider { provider ->
+                        val spy = spyForLogger<TestObject>()
+                        provider.addEvent(TestObject::class, event1())
 
-                    val sectionSpy = spy {
-                        provider.addEvent(ObjectA::class, SpiedEvent("Test 2", SpiedEvent.Level.DEBUG, null, emptyMap()))
+                        val sectionSpy = spy {
+                            provider.addEvent(TestObject::class, event2())
+                        }
+
+                        sectionSpy.events() should containExactly(event2())
                     }
-
-                    sectionSpy.events() should containExactly(SpiedEvent("Test 2", SpiedEvent.Level.DEBUG, null, emptyMap()))
                 }
             }
         }
 
         "spy by literal" - {
             "creates spy for literal" - {
-                val provider = FakeSpyProvider()
-                ServiceLoaderWrapper.predefine<SpyProvider>(provider)
+                useFakeSpyProvider { provider ->
+                    val spy = spyForLogger("a") {
+                        provider.addEvent("a", event())
+                    }
 
-                val spy = spyForLogger("a") {
-                    provider.addEvent("a", SpiedEvent("Test", SpiedEvent.Level.DEBUG, null, emptyMap()))
+                    spy.events() should containExactly(event())
                 }
-
-                spy.events() should containExactly(SpiedEvent("Test", SpiedEvent.Level.DEBUG, null, emptyMap()))
             }
 
             "closes underlying spy" - {
-                val provider = FakeSpyProvider()
-                ServiceLoaderWrapper.predefine<SpyProvider>(provider)
+                useFakeSpyProvider { provider ->
+                    spyForLogger("a") {
+                    }
 
-                spyForLogger("a") {
+                    provider.allInstancesFor("a").forAll {
+                        it.shouldBeClosed()
+                    }
                 }
-
-                provider.allInstancesFor("a").first() should beClosed()
             }
 
             "takes snapshot of recorded events" - {
-                val provider = FakeSpyProvider()
-                ServiceLoaderWrapper.predefine<SpyProvider>(provider)
+                useFakeSpyProvider { provider ->
+                    val spy = spyForLogger("a") {
+                        provider.addEvent("a", event1())
+                    }
+                    provider.addEvent("a", event2())
 
-                val spy = spyForLogger("a") {
-                    provider.addEvent("a", SpiedEvent("Test 1", SpiedEvent.Level.DEBUG, null, emptyMap()))
+                    spy.events() should containExactly(event1())
                 }
-                provider.addEvent("a", SpiedEvent("Test 2", SpiedEvent.Level.DEBUG, null, emptyMap()))
-
-                spy.events() should containExactly(SpiedEvent("Test 1", SpiedEvent.Level.DEBUG, null, emptyMap()))
             }
 
             "creates new spy for each request" - {
-                val provider = FakeSpyProvider()
-                ServiceLoaderWrapper.predefine<SpyProvider>(provider)
+                useFakeSpyProvider { provider ->
+                    spyForLogger("a") {
+                        provider.addEvent("a", event1())
+                        val spy = spyForLogger("a") {
+                            provider.addEvent("a", event2())
+                        }
 
-                spyForLogger("a") {
-                    provider.addEvent("a", SpiedEvent("Test 1", SpiedEvent.Level.DEBUG, null, emptyMap()))
-                    val spy = spyForLogger("a") {
-                        provider.addEvent("a", SpiedEvent("Test 2", SpiedEvent.Level.DEBUG, null, emptyMap()))
+                        spy.events() should containExactly(event2())
                     }
-
-                    spy.events() should containExactly(SpiedEvent("Test 2", SpiedEvent.Level.DEBUG, null, emptyMap()))
                 }
             }
 
             "delayed spying" - {
                 "creates spy for block" - {
-                    val provider = FakeSpyProvider()
-                    ServiceLoaderWrapper.predefine<SpyProvider>(provider)
-                    val spy = spyForLogger("a")
-                    provider.addEvent("a", SpiedEvent("Test 1", SpiedEvent.Level.DEBUG, null, emptyMap()))
+                    useFakeSpyProvider { provider ->
+                        val spy = spyForLogger("a")
+                        provider.addEvent("a", event1())
 
-                    val sectionSpy = spy {
-                        provider.addEvent("a", SpiedEvent("Test 2", SpiedEvent.Level.DEBUG, null, emptyMap()))
+                        val sectionSpy = spy {
+                            provider.addEvent("a", event2())
+                        }
+
+                        sectionSpy.events() should containExactly(event2())
                     }
-
-                    sectionSpy.events() should containExactly(SpiedEvent("Test 2", SpiedEvent.Level.DEBUG, null, emptyMap()))
                 }
             }
         }
     }
 }
 
+private fun useFakeSpyProvider(block: (FakeSpyProvider) -> Unit) {
+    val provider = FakeSpyProvider()
+    ServiceLoaderWrapper.predefine<SpyProvider>(provider)
+    block(provider)
+}
+
+private fun event() = SpiedEvent("Test", SpiedEvent.Level.DEBUG, null, emptyMap())
+private fun event1() = SpiedEvent("Test 1", SpiedEvent.Level.DEBUG, null, emptyMap())
+private fun event2() = SpiedEvent("Test 2", SpiedEvent.Level.DEBUG, null, emptyMap())
+
 private fun beClosed() = object : Matcher<FakeLogSpy> {
     override fun test(value: FakeLogSpy): MatcherResult {
         return MatcherResult(
             value.isClosed(),
-            {"Expected to be closed, but it's not."},
+            {"Expected to be closed, but it was not."},
             {"Expected to be not closed, but was."}
         )
     }
 
 }
 
-class ObjectA;
+private fun FakeLogSpy.shouldBeClosed() = this should beClosed()
 
-class ObjectB;
+class TestObject;
