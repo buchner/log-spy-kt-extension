72764b8f7faf6d7a8dc47742fe3b5751cc87e625
Bjoern Buchner <bjoern.buchner@torommo.net>
2020-08-15 14:05:55 +0200

    Use Coveralls for code coverage reporting

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,6 +19,7 @@ plugins {
     `java-library`
     `maven-publish`
     signing
+    id("com.github.nbaztec.coveralls-jacoco") version "1.0.5"
     id("tech.formatter-kt.formatter") version "0.4.11"
 }
 
@@ -140,6 +141,11 @@ subprojects {
         useInMemoryPgpKeys(signingKey, signingPassword)
         sign(publishing.publications["mavenJava"])
     }
+}
+
+
+coverallsJacoco {
+    reportPath = "${project.rootProject.buildDir}/reports/jacoco/report.xml"
 }
 
 fun gitVersion(default: String = "0.0.0"): String {
