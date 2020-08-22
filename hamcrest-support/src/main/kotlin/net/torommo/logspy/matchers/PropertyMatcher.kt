eb49717c6ccfa28d59e3ce3d659fa9ba7cd63bb1
Bjoern Buchner <bjoern.buchner@torommo.net>
2020-08-22 17:17:01 +0200

    Base PropertyMatcher on BaseMatcher
    Rebasing gives more control and flexibility.

--- hamcrest-support/src/main/kotlin/net/torommo/logspy/matchers/PropertyMatcher.kt
+++ hamcrest-support/src/main/kotlin/net/torommo/logspy/matchers/PropertyMatcher.kt
@@ -1,29 +1,52 @@
 package net.torommo.logspy.matchers
 
+import org.hamcrest.BaseMatcher
+import org.hamcrest.Description
+import org.hamcrest.Matcher
+import kotlin.reflect.KClass
 import kotlin.reflect.KFunction1
 import kotlin.reflect.KProperty1
-import org.hamcrest.FeatureMatcher
-import org.hamcrest.Matcher
 
-internal class PropertyMatcher<V, U>(
+internal class PropertyMatcher<V : Any, U>(
     private val extractor: (V) -> U,
-    propertyHost: String?,
-    propertyName: String,
-    matcher: Matcher<U>
-) : FeatureMatcher<V, U>(matcher, "a $propertyHost with $propertyName", propertyName) {
+    private val type: KClass<*>,
+    private val propertyName: String,
+    private val matcher: Matcher<U>
+) : BaseMatcher<V>() {
     companion object {
-        inline fun <reified V, U> property(property: KProperty1<V, U>, matcher: Matcher<U>):
+        inline fun <reified V : Any, U> property(property: KProperty1<V, U>, matcher: Matcher<U>):
             PropertyMatcher<V, U> {
-                return PropertyMatcher(property, V::class.simpleName, property.name, matcher)
+                return PropertyMatcher(property, V::class, property.name, matcher)
             }
 
-        inline fun <reified V, U> property(property: KFunction1<V, U>, matcher: Matcher<U>):
+        inline fun <reified V : Any, U> property(property: KFunction1<V, U>, matcher: Matcher<U>):
             PropertyMatcher<V, U> {
-                return PropertyMatcher(property, V::class.simpleName, property.name, matcher)
+                return PropertyMatcher(property, V::class, property.name, matcher)
             }
     }
 
-    override fun featureValueOf(actual: V): U {
-        return extractor(actual)
+    override fun describeTo(description: Description?) {
+        description?.appendText("a ${type.simpleName} with $propertyName ")
+        description?.appendDescriptionOf(matcher)
+    }
+
+    override fun describeMismatch(item: Any?, description: Description?) {
+        if (item == null) {
+            description?.appendText("was null")
+        }
+        if (type.isInstance(item) && item != null) {
+            description?.appendText("$propertyName ")
+            @Suppress("UNCHECKED_CAST")
+            matcher.describeMismatch(extractor(item as V), description)
+        }
+    }
+
+    override fun matches(actual: Any?): Boolean {
+        return if (type.isInstance(actual) && actual != null) {
+            @Suppress("UNCHECKED_CAST")
+            matcher.matches(extractor(actual as V))
+        } else {
+            false
+        }
     }
 }
