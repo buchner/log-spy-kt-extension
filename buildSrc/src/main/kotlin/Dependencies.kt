e044f16ebebc8d022505437915ed1deb4fef7629
Bjoern Buchner <bjoern.buchner@torommo.net>
2020-10-03 16:32:21 +0200

    Bump Kotest version

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -25,7 +25,7 @@ object Libraries {
 
 object TestLibraries {
     private object Versions {
-        const val kotest = "4.2.4"
+        const val kotest = "4.2.5"
         const val logstashEncoder = "6.4"
     }
 
