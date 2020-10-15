8c2d3d6829897b84b51fc06173d8d566ef872029
Bjoern Buchner <bjoern.buchner@torommo.net>
2020-10-15 08:18:01 +0200

    Bump Dokka plugin version

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,7 +13,7 @@ plugins {
     signing
     id("com.github.nbaztec.coveralls-jacoco") version "1.0.5"
     id("tech.formatter-kt.formatter") version "0.6.4"
-    id("org.jetbrains.dokka") version "1.4.0-rc"
+    id("org.jetbrains.dokka") version "1.4.10"
 }
 
 allprojects {
