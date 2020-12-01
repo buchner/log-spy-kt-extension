47b8155678a045a6d37ffda06f1a73eac8d2f899
Bjoern Buchner <bjoern.buchner@torommo.net>
2020-12-01 19:34:37 +0100

    Bump Kotlin autoformatter plugin version to 0.6.13

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,7 +12,7 @@ plugins {
     `maven-publish`
     signing
     id("com.github.nbaztec.coveralls-jacoco") version "1.0.5"
-    id("tech.formatter-kt.formatter") version "0.6.4"
+    id("tech.formatter-kt.formatter") version "0.6.13"
     id("org.jetbrains.dokka") version "1.4.10"
 }
 
